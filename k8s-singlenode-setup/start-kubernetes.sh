2c861b9d02ce514ea27780a6517b96675cc7d474
Ardalan Kangarlou <Ardalan.Kangarlou@netapp.com>
2016-10-20 16:50:24 -0400

    Added the presentation.

--- k8s-singlenode-setup/start-kubernetes.sh
+++ k8s-singlenode-setup/start-kubernetes.sh
@@ -11,7 +11,7 @@ docker run \
     --volume=/var/lib/docker/:/var/lib/docker:rw \
     --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
     --volume=/var/run:/var/run:rw \
-	--volume=/dev:/dev:rw \
+    --volume=/dev:/dev:rw \
     --net=host \
     --pid=host \
     --privileged=true \
